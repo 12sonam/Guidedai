c482240a7fd48d70309e07991f9773f4af2f28c2
12sonam <ssonamssherpa41@gmail.com>
2025-01-31 04:21:04 +0545

    footer and header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import React from "react"; 
+import React,{useRef, useEffect} from "react"; 
 import { Container, Row, Button } from 'reactstrap'
 import { NavLink, Link } from 'react-router-dom'
 
 import logo from '../../assets/images/logo.png'
 import "./header.css"
 
-const nav_links= [
+const nav__links= [
     {
         path:'/home',
         display:'Home'
@@ -18,10 +18,30 @@ const nav_links= [
         path:'/tours',
         display:'Tours'
     }, 
-]
+];
 
 const Header = () => {
-    return <header className="header">
+
+const headerRef = useRef(null)
+
+const stickyHeaderFunc = ()=> {
+    window.addEventListener('scroll', ()=>{
+        if(document.body.scrollTop >80 || document.documentElement.scrollTop >80){
+            headerRef.current.classList.add('sticky__header')
+        } else {
+            headerRef.current.classList.remove('sticky__header')
+        }
+    });
+};
+
+useEffect(()=>{
+    stickyHeaderFunc()
+
+    return window.removeEventListener('scroll', stickyHeaderFunc)
+});
+ 
+    return (
+    <header className="header"  ref={headerRef}>
         <Container>
             <Row>
                 <div className="nav__wrapper d-flex align-items-center justify-content-between">
@@ -36,7 +56,7 @@ const Header = () => {
                     <div className="navigation">
                         <ul className="menu d-flex align-items-center gap-5">
                             {
-                                nav_links.map((item,index)=>(
+                                nav__links.map((item,index)=>(
                                     <li className="nav__item" key={index}>
                                         <NavLink to={item.path} className={navClass=> navClass.isActive ? 'active__link' : ''}>{item.display}</NavLink>
                                     </li>
@@ -60,6 +80,7 @@ const Header = () => {
             </Row>
         </Container>
     </header>
+    );
 };
 
 export default Header;
